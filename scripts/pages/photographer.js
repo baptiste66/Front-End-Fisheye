518650ef44a9bfb41af73567413680d13eef57c5
baptiste66 <101480985+baptiste66@users.noreply.github.com>
2023-09-14 11:37:57 +0200

    likes/unlike

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,9 +4,9 @@ let id = params.get("id");
 
 const getPhotographers = async () => {
     try {
-        // récupérer des données à partir du JSON
+        
         const response = await fetch('./data/photographers.json'); 
-        // la réponse de la requête est transformée en données JSON 
+        
         const photographersData = await response.json();
         return photographersData;
     } catch (error) {
@@ -24,15 +24,14 @@ async function displayData(photographer) {
 }
 
 async function init() {
-    // Récupère les datas des photographes
+    
     const { photographers } = await getPhotographers();
     
-    // Filtrer le photographe en fonction de l'ID
     const photographer = photographers.find((p) => p.id.toString() === id);
 
     if (photographer) {
         displayData(photographer);
-        console.log(photographer);
+        
     } else {
         console.error(`Aucun photographe trouvé avec l'ID ${id}`);
     }
@@ -40,48 +39,27 @@ async function init() {
 init();
 
 
-
-
-
-
-const getSamplePhotos = async () => {
-    try {
-        // récupérer des données à partir du JSON
-        const response = await fetch('./data/photographers.json'); 
-        // la réponse de la requête est transformée en données JSON 
-        const mediaData = await response.json();
-        return mediaData;
-    } catch (error) {
-        console.error('Une erreur est survenue :', error);
-        return { media: [] };
-    }
-};
-
-
 async function displaycontent(medias) {
     const photographHeader = document.querySelector(".photographe-main");
     
-    // Effacer le contenu existant
     photographHeader.innerHTML = '';
 
     for (const media of medias) {
         const photographerModel = mediaTemplate(media);
+
         const mediaDOM = photographerModel.getContentDOM();
         photographHeader.appendChild(mediaDOM);
     }
 }
 
-
 async function inits() {
     // Récupère les données des médias
-    const { media } = await getSamplePhotos();
+    const { media } = await getPhotographers();
     
-    // Filtrer les médias en fonction de l'ID du photographe
     const medias = media.filter((c) => c.photographerId.toString() === id);
 
     if (medias.length > 0) {
         displaycontent(medias);
-        console.log(medias);
     } else {
         console.error(`Aucun contenu pour ce photographe ${id}`);
     }
--- scripts/templates/photographer-information.js
+++ scripts/templates/photographer-information.js
@@ -1,12 +1,12 @@
 let photographName =params.get("name");
 
-
 function photographerTemplate(data) {
-    const { name, portrait, country, city, price, tagline } = data;
+    let { name, portrait, country, city, price, tagline } = data;
 
    const contactModal = document.getElementById('contact_modal');
-   const modalTitle = contactModal.querySelector('h2');
+   let modalTitle = contactModal.querySelector('h2');
    modalTitle.textContent = `Contactez-moi ${name}`;
+   modalTitle.setAttribute('class', 'modalTitle')
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -38,7 +38,7 @@ function photographerTemplate(data) {
 
         let priceParagraph = document.createElement('p');
         priceParagraph.textContent = price + '€/jour';
-        priceParagraph.setAttribute('class', 'price');
+        priceParagraph.setAttribute('class', 'photographer-price');
         priceParagraph.setAttribute('aria-label', `Prix: ${price} par jour`);
 
         let taglineParagraph = document.createElement('p');
@@ -61,38 +61,93 @@ function photographerTemplate(data) {
     return { name, picture, getUserCardDOM };
 }
 
-
+let totalLikes = 0;
+let totalLikesElement;
+const likedImages = new Set();
 
 function mediaTemplate(dataContent) {
-    let { image, title, likes } = dataContent;
+    let { image, title, likes, video } = dataContent;
     let pictureContent = `Sample Photos/${photographName}/${image}`;
-   
+    let likesNumber = parseInt(likes);
+
+    totalLikes += likesNumber; // Ajoutez les likes de cet élément au total
 
     function getContentDOM() {
         const article = document.createElement('article');
-        article.setAttribute('class', 'template_content')
+        article.setAttribute('class', 'template_content');
 
         const description = document.createElement('span');
-        description.setAttribute('class', 'photographe-main_picture_description') 
-
-        let img = document.createElement('img');
-        img.setAttribute('src', pictureContent);
-        img.setAttribute('class', 'photographe-main_picture');
+        description.setAttribute('class', 'photographe-main_picture_description');
 
         let h2 = document.createElement('h2');
         h2.textContent = title;
 
         let likesContent = document.createElement('p');
-        likesContent.innerHTML = `${likes} <i class="fas fa-heart"></i>`;
+        likesContent.innerHTML = `${likesNumber} `;
+        likesContent.setAttribute('aria-label', `likes`);
 
-        article.appendChild(img);
-        article.appendChild(description)
-        
+        if (video) {
+            const videoElement = document.createElement('video');
+            videoElement.setAttribute('class', 'photographe-main_video');
+            videoElement.setAttribute('controls', 'controls');
+
+            const source = document.createElement('source');
+            source.setAttribute('src', `Sample Photos/${photographName}/${video}`);
+            source.setAttribute('type', 'video/mp4');
+
+            videoElement.appendChild(source);
+            article.appendChild(videoElement);
+        } else {
+            let img = document.createElement('img');
+            img.setAttribute('src', pictureContent);
+            img.setAttribute('class', 'photographe-main_picture');
+            article.appendChild(img);
+        }
+
+        // Créez un bouton pour le like/unlike
+        const likeButton = document.createElement('button');
+        likeButton.innerHTML = '<i class="fas fa-heart"></i>';
+        likeButton.className = likedImages.has(image) ? 'liked' : ''; // Applique la classe 'liked' si l'image est déjà likée
+
+        likeButton.addEventListener('click', () => {
+            if (!likedImages.has(image)) {
+                likedImages.add(image);
+                likesNumber += 1;
+                likesContent.innerHTML = `${likesNumber} `;
+                totalLikes += 1;
+                totalLikesElement.innerHTML = `${totalLikes} <i class="fas fa-heart"></i>`;
+                likeButton.classList.add('liked');
+            } else {
+                likedImages.delete(image);
+                likesNumber -= 1;
+                likesContent.innerHTML = `${likesNumber} `;
+                totalLikes -= 1;
+                totalLikesElement.innerHTML = `${totalLikes} <i class="fas fa-heart"></i>`;
+                likeButton.classList.remove('liked');
+            }
+        });
+
+        // Ajoutez le bouton like/unlike à l'article
+        article.appendChild(likeButton);
+
+        article.appendChild(description);
+
         description.appendChild(h2);
         description.appendChild(likesContent);
 
         return article;
     }
 
+    if (!totalLikesElement) {
+        totalLikesElement = document.createElement('p');
+        totalLikesElement.setAttribute('class', 'photographer-TotalLikes');
+        document.querySelector(".photograph-header").appendChild(totalLikesElement);
+    }
+
+    totalLikesElement.innerHTML = `${totalLikes} <i class="fas fa-heart"></i>`;
+
     return { getContentDOM };
 }
+
+
+
